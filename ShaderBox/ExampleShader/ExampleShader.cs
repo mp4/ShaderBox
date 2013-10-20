2340c77c283ecf08dac1b89161e6fe40e30b7f6d
Marsh Poulson IV <marsh@linux-7i17.site>
2013-10-19 19:30:30 -0600

    full screen now covered by two triangles

--- ShaderBox/ExampleShader/ExampleShader.cs
+++ ShaderBox/ExampleShader/ExampleShader.cs
@@ -44,6 +44,9 @@ namespace ExampleShader
 				float[] points = {
 					-1.0f, 1.0f, -0.1f,
 					-1.0f, -1.0f, -0.1f,
+					1.0f, -1.0f, -0.1f ,
+					1.0f, 1.0f, -0.1f,
+					-1.0f, 1.0f, -0.1f,
 					1.0f, -1.0f, -0.1f
 				};
 				var vbo = GL.GenBuffer ();
